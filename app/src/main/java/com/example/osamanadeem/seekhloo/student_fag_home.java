d34c8040f87d1b018f17adbc0d649043875a7333
obutt8019@gmail.com <fauacsk1122vrockpokey>
2019-06-11 00:42:27 +0500

    classroom created

--- app/src/main/java/com/example/osamanadeem/seekhloo/student_fag_home.java
+++ app/src/main/java/com/example/osamanadeem/seekhloo/student_fag_home.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class student_fag_home extends Fragment {
+
+    private ImageView imageView;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.student_frag_home,container,false);
+
+
         return v;
 
     }
